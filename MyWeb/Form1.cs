eedceb6a7c3a8db9207fc488b1e69e5fa8739581
Юрий Соломон <tsar.povelevaet@gmail.com>
2022-09-01 00:51:35 +0300

    Добавьте файлы проекта.

---
+++ MyWeb/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabPage1.Text = "1";
+            tabPage2.Text = "2";
+            tabPage3.Text = "3";
+        }
+    }
+}
